0a0ff258998631bb78a76fb41fba20af5819a93a
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2022-03-29 17:50:25 +0530

    ok

--- src/engine/physics.ts
+++ src/engine/physics.ts
@@ -1,5 +1,23 @@
 import {drawer} from "./drawer";
 import { objects } from "./drawer";
-export default class physics extends drawer{
-    
+export default class physics{
+    public  gravity:number=9;
+
+    public static iscolliding(object1,object2):boolean{
+        var center_dis=object1.radius+object2.radius;
+        var real_dis=Math.pow((Math.pow(object1.centerx-object2.centerx,2))+(Math.pow(object1.centery-object2.centery,2)),1/2);
+        
+        if(real_dis<=center_dis){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    public gravitational_force(){
+        
+    }
+
 }
+
